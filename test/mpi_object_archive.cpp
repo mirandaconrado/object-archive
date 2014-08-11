85be17dfbfac91b9e3c7218c9fdf46a05504e108
Conrado Miranda <miranda.conrado@gmail.com>
2014-08-11 08:49:37 -0300

    [mpi] Added test for recording everything.

--- test/mpi_object_archive.cpp
+++ test/mpi_object_archive.cpp
@@ -44,3 +44,59 @@ TEST(MPIObjectArchiveTest, InsertLoad) {
 
   world.barrier();
 }
+
+TEST(MPIObjectArchiveTest, RecordEverything) {
+  MPIObjectArchive<size_t>* ar = new MPIObjectArchive<size_t>(&world,
+      world.rank() == 0);
+  world.barrier();
+
+  if (world.rank() != 0)
+    ar->insert(world.rank(), world.rank()+5);
+
+  world.barrier();
+
+  // Loops around to ensure there's enough time for processes to communicate
+  for (int i = 0; i < 1000; i++)
+    ar->mpi_process();
+  world.barrier();
+
+  if (world.rank() != 0)
+    delete ar;
+  world.barrier();
+
+  if (world.rank() == 0)
+    for (int i = 1; i < world.size(); i++) {
+      int val;
+      ar->load(i, val);
+      EXPECT_EQ(i+5, val);
+    }
+
+  world.barrier();
+}
+
+TEST(MPIObjectArchiveTest, RecordEverythingFail) {
+  MPIObjectArchive<size_t>* ar = new MPIObjectArchive<size_t>(&world,
+      world.rank() == 0);
+  world.barrier();
+
+  if (world.rank() != 0)
+    ar->insert(world.rank(), world.rank()+5);
+  world.barrier();
+
+  ar->mpi_process();
+  // Don't stop here and let the data source die, so we can check if we
+  // correctly handle failed requests
+
+  if (world.rank() != 0)
+    delete ar;
+  world.barrier();
+
+  if (world.rank() == 0)
+    for (int i = 1; i < world.size(); i++) {
+      int val;
+      ar->load(i, val);
+      EXPECT_NE(i+5, val);
+    }
+
+  world.barrier();
+}
